2aec414f6963fe74433a2d1b7e5dba7729e8ead5
Palak0206 <palakrawat0206@gmail.com>
2025-03-19 12:16:22 +0530

    Initial commit

---
+++ Cistron-main/src/main/java/com/example/Cistron_main/CistronMainApplication.java
@@ -0,0 +1,10 @@
+package com.example.Cistron_main;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CistronMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CistronMainApplication.class, args);
+    }
+}
